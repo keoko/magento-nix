5a1c87c2ee1e95bfd48af624e957984e3e5032ec
Natxo Cabré <cabre@adobe.com>
2021-12-25 13:39:21 +0100

    init project

---
+++ shell.nix
@@ -0,0 +1,12 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.bash
+    pkgs.php74
+    pkgs.php74Packages.composer
+    pkgs.nginx
+  ];
+}
