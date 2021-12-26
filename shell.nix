f829e8f6309049dd51daaed11516391fd5b37820
Natxo Cabré <cabre@adobe.com>
2021-12-26 21:15:39 +0100

    pass memory_limit parameter for PHP

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  php74' = pkgs.php74.buildEnv { 
+    extensions = { enabled, all }: enabled ++ [ all.xsl ];
+    extraConfig = "memory_limit = -1"; };
 in
 pkgs.mkShell {
   buildInputs = [
     pkgs.bash
-    (pkgs.php74.withExtensions ({ enabled, all }: enabled ++ [ all.xsl ]))
+    php74'
     pkgs.php74Packages.composer
     pkgs.nginx
     pkgs.mysql80
