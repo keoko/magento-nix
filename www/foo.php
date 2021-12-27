1b100021338c8a0c33da58960ca230bdb424f483
Natxo Cabré <cabre@adobe.com>
2021-12-27 16:58:56 +0100

    removed unused folder

--- www/foo.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=testdb;port=3306;charset=utf8mb4', 'root', 'root');
-
-$sql = 'SELECT orderid FROM orders ORDER BY orderid';
-foreach ($db->query($sql) as $row) {
-    print $row['orderid'] . "\n";
-}
